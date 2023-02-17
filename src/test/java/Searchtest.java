34b36db4321811a11b35fbefd1419bea3835af29
Dmitrii Grebeniuk <dmtrgrbnk@gmail.com>
2023-02-17 19:25:58 +0300

    duble

--- src/test/java/Searchtest.java
+++ src/test/java/Searchtest.java
@@ -11,6 +11,7 @@ public class SearchTests {
       open("https://www.google.com/");
       $("[name=q]").setValue("selenide").pressEnter();
       $("[id=search]").shouldHave(text("https://selenide.org"));
+      $("[id=search]").shouldHave(text("https://selenide.org"));
    }
 
 }
